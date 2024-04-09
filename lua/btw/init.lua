368ea4e99663ec6244de4baa7b60b1c14a20cd88
letieu <letieu8@gmail.com>
2024-04-09 16:28:03 +0700

    fix: open btw if nothing render

--- lua/btw/init.lua
+++ lua/btw/init.lua
@@ -58,6 +58,10 @@ H.create_autocommands = function()
 	local augroup = vim.api.nvim_create_augroup("Btw", {})
 
 	local on_vimenter = function()
+		if H.is_something_shown() then
+			return
+		end
+
 		H.is_in_vimenter = true
 		Btw.open()
 	end
@@ -168,6 +172,40 @@ H.create_lines = function(text)
 	end
 
 	return lines
+end
+
+H.is_something_shown = function()
+	-- Don't open Starter buffer if Neovim is opened to show something. That is
+	-- when at least one of the following is true:
+	-- - There are files in arguments (like `nvim foo.txt` with new file).
+	if vim.fn.argc() > 0 then
+		return true
+	end
+
+	-- - Several buffers are listed (like session with placeholder buffers). That
+	--   means unlisted buffers (like from `nvim-tree`) don't affect decision.
+	local listed_buffers = vim.tbl_filter(function(buf_id)
+		return vim.fn.buflisted(buf_id) == 1
+	end, vim.api.nvim_list_bufs())
+	if #listed_buffers > 1 then
+		return true
+	end
+
+	-- - Current buffer has any lines (something opened explicitly).
+	-- NOTE: Usage of `line2byte(line('$') + 1) < 0` seemed to be fine, but it
+	-- doesn't work if some automated changed was made to buffer while leaving it
+	-- empty (returns 2 instead of -1). This was also the reason of not being
+	-- able to test with child Neovim process from 'tests/helpers'.
+	local n_lines = vim.api.nvim_buf_line_count(0)
+	if n_lines > 1 then
+		return true
+	end
+	local first_line = vim.api.nvim_buf_get_lines(0, 0, 1, true)[1]
+	if string.len(first_line) > 0 then
+		return true
+	end
+
+	return false
 end
 
 return Btw
